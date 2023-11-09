8e5bf3086462e6a6eb35b2172dbd190528bf2066
giovannaloro <giovanniannaloro3x5@gmail.com>
2023-11-09 13:55:59 +0100

    Added Adaboost hyperparameter tuning and models crossvalidation

--- hyperparameter_tuning/sgd_hyperparameter_tuning.py
+++ hyperparameter_tuning/sgd_hyperparameter_tuning.py
@@ -3,15 +3,11 @@ import os
 import scipy 
 import numpy as np
 from sklearn.datasets import make_classification
-from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import f1_score, accuracy_score
-from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import SGDClassifier
-from sklearn.svm import SVC
-from sklearn.svm import NuSVC
-from sklearn.tree import DecisionTreeClassifier
+
 
 #uplouading dataset 
 os.chdir('..')
@@ -31,24 +27,16 @@ df = pandas.read_csv('preprocessed_dataset/label_encoding/ML_MED_Dataset_test_pr
 X_test = df.iloc[:,1:17]
 y_test = df.iloc[:,17:22]
 y_test = np.ravel(y_test)
-#base estimators best_params
-svc_best_params = {'C': 10, 'gamma': 0.01, 'kernel': 'rbf'}
-sgd_best_params = {'alpha': 0.01, 'loss': 'modified_huber', 'penalty': 'l2'}
-dtc_best_params = {'criterion': 'entropy', 'splitter': 'best'}
 #training and hyperparameters tuning
-gnb = GaussianNB()
-svc = SVC()
 sgd = SGDClassifier()
-dtc = DecisionTreeClassifier()
-distribution_params = {'estimator':[gnb,svc,sgd,dtc],'n_estimators':scipy.stats.poisson(loc=200, mu=75), 'algorithm':['SAMME'],'learning_rate':scipy.stats.uniform(loc=0, scale=2)}
-adb = AdaBoostClassifier()
-print("start \n")
-adb_randomized = RandomizedSearchCV(estimator=adb, param_distributions=distribution_params, n_jobs=-1, n_iter=200, cv=8,verbose=3)
-adb_randomized.fit(X_train, y_train)
-print(adb_randomized.best_params_)
-adb = AdaBoostClassifier(**adb_randomized.best_params_)
-adb.fit(X_train,y_train)
-y_pred = adb.predict(X_validation)
+param_grid = {'loss':['hinge', 'log_loss', 'modified_huber', 'squared_hinge', 'perceptron', 'squared_error', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive'],
+              'penalty':['l2', 'l1', 'elasticnet', None],'alpha':[0.0001,0.001,0.001,0.01]} 
+grid = GridSearchCV(sgd,param_grid, n_jobs=-1)
+grid.fit(X_train,y_train)
+print(grid.best_params_)
+sgs = SGDClassifier(**grid.best_params_)
+sgd.fit(X_train,y_train)
+y_pred = sgd.predict(X_validation)
 f_one_score = f1_score(y_validation, y_pred, average="weighted")
 accuracy = accuracy_score(y_validation, y_pred)
 print("f1 score:", f_one_score)
